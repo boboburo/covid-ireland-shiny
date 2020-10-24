47de24f143911f9f6a3ee10e5b829dbc08ceb3ca
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-24 12:13:16 +0100

    tests files update

--- tests/testthat/test-fct_plot_bottom.R
+++ tests/testthat/test-fct_plot_bottom.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+df <- load_covid_ireland(src = "offline_sqlite")
+
+test_that("Plot generated", {
+  plot <- plot_btm(df, y_val = "plt1_x",
+           county = "Dublin",
+           end_date = ymd("2020-05-29"), days_before =30, type = "line")
+  
+  expect_s3_class(plot,"ggplot")
 })
+
+
+
