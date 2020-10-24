c971f4543c7a0ce1d664e901a1d481df96f3d07a
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-24 10:13:17 +0100

    Alter testthat file to not look for package

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
-library(covid-ireland-shiny)
 
-test_check("covid-ireland-shiny")
+test_dir(
+  "./testthat",
+  # Run in the app's environment containing all support methods.
+  env = shiny::loadSupport(),
+  # Display the regular progress output and throw an error if any test error is found
+  reporter = c("progress", "fail")
+)
