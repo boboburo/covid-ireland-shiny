7a8cd8d662649040813a5313f73afc4a4819b849
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-24 12:14:17 +0100

    Files updates

--- R/global.R
+++ R/global.R
@@ -1,2 +1,13 @@
 #Load the latest daily cases 
-dailycases <- load_covid_ireland()
+
+run_mode = "offline"
+
+if(run_mode == "online"){
+  dailycases <- load_covid_ireland(src = "online_csv")
+}
+  
+if(run_mode == "offline") {
+  dailycases <- load_covid_ireland(src = "offline_sqlite")
+}
+  
+
