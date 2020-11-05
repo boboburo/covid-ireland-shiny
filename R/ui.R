f2cfc6b3b12310f3b7f78acf8315f3460c652d5c
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-11-05 09:24:27 +0000

    Button and instructions

--- R/ui.R
+++ R/ui.R
@@ -1,6 +1,7 @@
 # Define UI for application that draws a histogram
 ui <-  shinyUI(
   fluidPage(#theme = shinythemes::shinytheme("flatly"),
+    #useShinydashboard(),
             tags$title("Analyze COVID Numbers in Ireland by County"),
             column(width = 12,
                    h1("COVID Numbers in Ireland by County"),
