a2ee493e5c53782d3afac5bc4819f551b0a7b542
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-11-14 18:31:12 +0000

    New data source and use SQLite DB

---
+++ DATA/get-data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl "https://storage.covid19datahub.io/data-1.csv" | sqlite-utils insert --csv covid.db data1 -
+curl "https://storage.covid19datahub.io/data-2.csv" | sqlite-utils insert --csv covid.db data2 -
+#curl "https://storage.covid19datahub.io/data-3.csv" | sqlite-utils insert --csv covid.db data3 -
+
